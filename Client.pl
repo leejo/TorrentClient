e1b47f46b23e82005b4f0cfbdc755bb092f52970
RKRT <mesg.raj@gmail.com>
2019-11-29 16:05:24 +0530

    29 Nov 2019

--- Client.pl
+++ Client.pl
@@ -14,31 +14,44 @@ use LWP::Simple qw(get);
 #binmode STDOUT, ":encoding(UTF-8)";
 
 # TODO
-# First make it work for ubuntu torrent, then make it work for any other torrent
+# 1. make it work for ubuntu torrent
 
 fun torrent_file_content($file) {
     my $contents;
-    open(my $fh, '<', $file) or die "Cannot open torrent file";
+    open( my $fh, '<', $file ) or die "Cannot open torrent file";
     {
-        local $/; $contents = <$fh>;
+        local $/;
+        $contents = <$fh>;
     }
     close($fh);
     return $contents;
 }
 
-
 my $torrent_file = 'ubuntu-18.04.3-desktop-amd64.iso.torrent';
-my $torrent = bdecode(torrent_file_content($torrent_file));
+my $torrent      = bdecode( torrent_file_content($torrent_file) );
 
-my $info_hash = Encode::encode("ISO-8859-1", sha1(bencode($torrent->{info})));
-my $announce = $torrent->{'announce'};
-my $port = 6881;
-my $left = $torrent->{'info'}->{'length'};
-my $uploaded = 0;
+my $info_hash  = Encode::encode( "ISO-8859-1", sha1( bencode( $torrent->{info} ) ) );
+my $announce   = $torrent->{'announce'};
+my $port       = 6881;
+my $left       = $torrent->{'info'}->{'length'};
+my $uploaded   = 0;
 my $downloaded = 0;
-my $peer_id = "-AZ2200-6wfG2wk6wWLc";
+my $peer_id    = "-AZ2200-6wfG2wk6wWLc";
 
-my $thr = $announce. "?info_hash=" . $info_hash . "&peer_id=" . $peer_id . "&port=" . $port . "&uploaded=" . $uploaded . "&downloaded=" . $downloaded . "&left=" . $left;
+my $thr =
+    $announce
+  . "?info_hash="
+  . $info_hash
+  . "&peer_id="
+  . $peer_id
+  . "&port="
+  . $port
+  . "&uploaded="
+  . $uploaded
+  . "&downloaded="
+  . $downloaded
+  . "&left="
+  . $left;
 
 my $response = get($thr) or die "Cannot connect";
 say Dumper bdecode($response);
