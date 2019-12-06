c66400e5aa1789e0fdd28514532e77cebb0a281e
RKRT <mesg.raj@gmail.com>
2019-12-06 13:23:55 +0530

    6 Dec 2019

--- Client.pl
+++ Client.pl
@@ -17,7 +17,7 @@ use LWP::Simple qw(get);
 
 
 # TODO:
-# 1. make it work for ubuntu torrent
+# 1. make it work for single file torrent
 
 fun torrent_file_content($file) {
     my $contents;
@@ -33,7 +33,11 @@ fun torrent_file_content($file) {
 my $torrent_file = 'ubuntu-18.04.3-desktop-amd64.iso.torrent';
 my $torrent      = bdecode( torrent_file_content($torrent_file) );
 
-my $info_hash  = Encode::encode( "ISO-8859-1", sha1( bencode( $torrent->{info} ) ) );
+my $file_name = $torrent->{'info'}->{'name'};
+my $file_length = $torrent->{'info'}->{'length'};
+my $piece_length = $torrent->{'info'}->{'piece length'};
+
+my $info_hash  = Encode::encode( "ISO-8859-1", sha1( bencode( $torrent->{'info'} ) ) );
 my $announce   = $torrent->{'announce'};
 my $port       = 6881;
 my $left       = $torrent->{'info'}->{'length'};
@@ -41,7 +45,7 @@ my $uploaded   = 0;
 my $downloaded = 0;
 my $peer_id    = "-AZ2200-6wfG2wk6wWLc";
 
-my $thr =
+my $tracker_request =
     $announce
   . "?info_hash="
   . $info_hash
@@ -56,7 +60,7 @@ my $thr =
   . "&left="
   . $left;
 
-my $response = get($thr) or die "Cannot connect to tracker";
+my $response = get($tracker_request) or die "Cannot connect to tracker";
 my $tracker_response = bdecode($response);
 
 my $peers = $tracker_response->{'peers'};
@@ -66,7 +70,7 @@ my $peers = $tracker_response->{'peers'};
 my $pstr = "BitTorrent protocol";
 my $message = pack 'C1A*a8a20a20', length($pstr), $pstr, '',  $info_hash, $peer_id;
 
-my $bitfields_num = length($torrent->{info}->{pieces}) / 20;
+my $bitfields_num = length($torrent->{'info'}->{'pieces'}) / 20;
 my $bitfield_num_bytes = 4 + 1 + ceil($bitfields_num / 8);
 
 # length - 4 bytes, id - 1 bytes,
@@ -83,10 +87,10 @@ my $data_channel = new Coro::Channel;
 # {piece_num, piece_data, piece_percent, piece_size}
 
 # TODO:
-# get piece number from piece_channel,
+# get piece index from piece_channel,
 # download and put in $data_channel,
 # if piece doesnt exists on the peer,
-# put piece number back on to piece_channel,
+# put piece index back on to piece_channel,
 # other worker may download it.
 
 for my $n (0..5) {
@@ -104,18 +108,65 @@ for my $n (0..5) {
         my ($pstr_r, $reserved_r, $info_hash_r, $peer_id_r, $c) = unpack 'C/a a8 a20 a20 a*', $buf;
         my ($bitfield_length, $bitfield_id, $bitfield_data) = unpack 'N1 C1' . ' B' . $bitfields_num, $bitfield;
 
+        my @bitfield_array = split("", $bitfield_data);
+
         if( $info_hash eq $info_hash_r ) {
-            my $interested =  pack('Nc', 1, 2);
+            my $interested = pack('Nc', 1, 2);
             my $choke_buf;
 
             $fh->syswrite($interested);
             $fh->sysread($choke_buf, 5);
 
-            my ($len, $id) = unpack 'Nc', $choke_buf;
-            if( $id == 1 ) {    # Got Unchoke
+            my ($length, $id) = unpack 'Nc', $choke_buf;
+            if( $id == 1 ) {
+                # Got Unchoke
+
+                PIECELOOP: {
+                    my $block_length = 2 ** 14;
+                    my $num_blocks = ceil($piece_length / $block_length);
+
+                    my $piece_index = $piece_channel->get;
+
+                    if( $bitfield_array[$piece_index] eq '1' ) {
+                        # piece exists on the peer
+                        if( $piece_index == $bitfields_num ) {
+                            # handle last piece
+                            # ...
+                        }
+                        else {
+                            my $block_counter = 0;
+                            my $piece_data;
+                            my $piece_offset = 0;
+                            my $request_pack;
+                            my $request;
+
+                            BLOCKLOOP: {
+                                my $block_buf;
+                                my $block_buf_size = 4 + 1 + 4 + 4 + ($block_length / 8);
+
+                                $request_pack = pack 'NNN', $piece_index, $piece_offset, $block_length;
+                                $request = pack 'Nca*', length($request_pack) + 1, 6, $request_pack;
+
+                                $fh->syswrite($request);
+                                $fh->sysread($block_buf, $block_buf_size);
+                                # ...
+                                # goto BLOCKLOOP;
+                            }
+                        }
+                    }
+                    else {
+                        # put back piece_index on piece channel
+                        $piece_channel->put($piece_index);
+                        say "going back to loop";
+                        goto PIECELOOP;
+                    }
+                }
+            }
+            elsif( $id == 0 ) {
+                # Got Choke
+                # prepare to end connection
                 # ...
             }
-
         }
     };
 }
